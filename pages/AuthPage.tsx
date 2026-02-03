f221b177c4c5c453fd31ea3daacf36b22034bfe0
Kaleemcodr <thurmondalex3@gmail.com>
2026-02-03 23:40:56 +0500

    Create AuthPage.tsx

---
+++ pages/AuthPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AuthPage() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Auth Page</h1>
+      <p>Deployment fixed ✅</p>
+    </div>
+  );
+}
